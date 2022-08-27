0985618d8f92d048968c9db19c63e4a1b2d2b870
fjohngaughan <fjohngaughan@gmail.com>
2022-08-27 00:08:13 +0000

    1st attempt at AddressParse.get()

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -13,12 +13,26 @@ class Home(TemplateView):
 class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
-    def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        # How do I grab the user input?
+        # How do I return the parsed components to the frontend?
+        # What errors do I need to account for?
+            # Invalid addresses structures
+            # inputs with just a space
+            # Inputs with characters that shouldn't be in an address? (maybe /, <, >, [], %, etc.)
+    def get(self, request):
+        address = request.GET.get("address")
+        address_components, address_type = self.parse(address)
+        context = {
+            "address_components": address_components,
+            "address_type": address_type,
+        }
+        data = json.dumps(context, sort_keys=True, default=str)
+        return HttpResponse(data, content_type='application/json')
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+        address_components, address_type = usaddress.tag(address)
         return address_components, address_type
